8ea04fc954eee30e9014f142840aa6ec68aa38ea
Apoi <49585634+kingofwhiterock@users.noreply.github.com>
2020-12-03 23:46:08 +0900

    Add: create signup_view function

--- reviewpost/urls.py
+++ reviewpost/urls.py
@@ -2,6 +2,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from reviewpost.views import signup_view
+
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls)
+    path('signup/', signup_view, name='signup'),
 ]
--- reviewpost/views.py
+++ reviewpost/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def signup_view(request):
+    context = {'data1': 100}
+    return render(request, 'signup.html', context)
